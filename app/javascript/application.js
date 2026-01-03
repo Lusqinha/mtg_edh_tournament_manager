59a1156e9f2a03b41b73a99ddcc37111cc9323fb
Lucas Borges <lucas18sborges@gmail.com>
2026-01-03 19:11:52 -0300

    feat: adicionar suporte a service worker e atualizar manifest da PWA

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// Register the service worker
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/service-worker")
+    .then((registration) => {
+      console.log("Service worker registered:", registration);
+    })
+    .catch((error) => {
+      console.error("Service worker registration failed:", error);
+    });
+}
